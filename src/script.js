505c1de5045cf2983a0c7fadca494339fe90bea0
brew4242564 <dlvera.py@gmail.com>
2026-01-03 19:22:58 -0300

    Add modal and close button javascript

--- src/script.js
+++ src/script.js
@@ -1,42 +1,68 @@
 const albums = [];
 const table = document.querySelector("#table-body");
+const btns = document.querySelector(".btns");
+const modal = document.querySelector("#modal-container");
+const form = document.querySelector("#modal");
 const emptyAlbum = {
-    title: "",
-    artist: "",
-    year: "",
-    genre: "",
-    played: false
+  title: "",
+  artist: "",
+  year: "",
+  genre: "",
+  played: false,
 };
 
-
 function renderTable() {
-    table.innerHTML = "";
+  table.innerHTML = "";
 
-    albums.forEach((item, index)=>{
-        const row = document.createElement("tr");
-        row.innerHTML = `
+  albums.forEach((item, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
         <td>${item.title}</td>
         <td>${item.artist}</td>
         <td>${item.year}</td>
         <td>${item.genre}</td>
         <td>${item.played}</td>
         `;
-        table.appendChild(row);
-    });
+    table.appendChild(row);
+  });
 }
+
+btns.addEventListener("click", (e) => {
+  if(e.target.tagName !== "BUTTON"){
+      return;
+  }
+  const { action } = e.target.dataset;
+  if (action === "add") {
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+  }
+  if (action === "edit") {
+    console.log("edit");
+  }
+  if (action === "delete") {
+    console.log("delete");
+  }
+});
+
+form.addEventListener("click", (e) => {
+  if (e.target.dataset.action === "close") {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+  }
+});
+
 
 
 // test
 
 const newAlbum = {
-    ...emptyAlbum,
-    title: "nevermind",
-    artist: "Nirvana",
-    year: 1991,
-    genre: "Grunge",
-    played: false
-}
-
+  ...emptyAlbum,
+  title: "nevermind",
+  artist: "Nirvana",
+  year: 1991,
+  genre: "Grunge",
+  played: false,
+};
 
 albums.push(newAlbum);
 
